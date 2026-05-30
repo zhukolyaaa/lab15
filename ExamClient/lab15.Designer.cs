f9f169a12682881cacacec9ce7694a213b243a8c
Ольга Жуковська <olha@example.com>
2026-05-31 01:06:28 +0300

    1

---
+++ ExamClient/lab15.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ExamClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.тестиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.довідкаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.початиТестToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.результатиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проПрограмуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.тестиToolStripMenuItem,
+            this.довідкаToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(282, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вихідToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.файлToolStripMenuItem.Text = "файл";
+            // 
+            // тестиToolStripMenuItem
+            // 
+            this.тестиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.початиТестToolStripMenuItem,
+            this.результатиToolStripMenuItem});
+            this.тестиToolStripMenuItem.Name = "тестиToolStripMenuItem";
+            this.тестиToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.тестиToolStripMenuItem.Text = "тести";
+            this.тестиToolStripMenuItem.Click += new System.EventHandler(this.тестиToolStripMenuItem_Click);
+            // 
+            // довідкаToolStripMenuItem1
+            // 
+            this.довідкаToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проПрограмуToolStripMenuItem});
+            this.довідкаToolStripMenuItem1.Name = "довідкаToolStripMenuItem1";
+            this.довідкаToolStripMenuItem1.Size = new System.Drawing.Size(73, 24);
+            this.довідкаToolStripMenuItem1.Text = "довідка";
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.вихідToolStripMenuItem.Text = "вихід";
+            // 
+            // початиТестToolStripMenuItem
+            // 
+            this.початиТестToolStripMenuItem.Name = "початиТестToolStripMenuItem";
+            this.початиТестToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.початиТестToolStripMenuItem.Text = "почати тест";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // результатиToolStripMenuItem
+            // 
+            this.результатиToolStripMenuItem.Name = "результатиToolStripMenuItem";
+            this.результатиToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.результатиToolStripMenuItem.Text = "результати";
+            // 
+            // проПрограмуToolStripMenuItem
+            // 
+            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
+            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.проПрограмуToolStripMenuItem.Text = "Про програму";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem тестиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem довідкаToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem початиТестToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem результатиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проПрограмуToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ ExamDataLib1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamDataLib
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+    }
+}
+namespace ExamDataLib
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; }
+    }
+}
